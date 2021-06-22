ed5e9767b7d80595b843408cca62d8d962c321d5
sandeepb20 <sandeepskr1721@gmail.com>
2021-06-22 18:36:27 +0530

    go

--- go/hello.go
+++ go/hello.go
@@ -1,6 +1,16 @@
 package main
-import "fmt"
 
-func main()  {
-	fmt.PrintLn("HEllo World")
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("HEllo World")
+	fmt.Println("The time is", time.Now())
+	fmt.Println("A random number", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(math.Pi)
 }
